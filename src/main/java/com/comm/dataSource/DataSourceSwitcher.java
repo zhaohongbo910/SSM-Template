f7c41676a987bec92844199479c2af34e4a73e5f
zhaohongbo910 <zhaohongbo910@163.com>
2024-01-05 15:02:33 +0800

    SSM template save

---
+++ src/main/java/com/comm/dataSource/DataSourceSwitcher.java
@@ -0,0 +1,58 @@
+package com.comm.dataSource;
+
+/**
+ * @Author buding
+ * @Description //TODO
+ * @Date 10:48 下午 2020/8/17
+ * @Param
+ * @return
+ **/
+public class DataSourceSwitcher {
+
+	private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>();
+
+	public static void setDataSource(String dataSource) {
+	    contextHolder.set(dataSource);
+	}
+
+	public static void setMaster() {
+		try {
+			clearDataSource();
+			setDataSource("master");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	public static void setSlave() {
+		try {
+			clearDataSource();
+			setDataSource("slave");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	public static void setLogMaster() {
+		try {
+			clearDataSource();
+			setDataSource("logMaster");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	public static void setLogSlave() {
+		try {
+			clearDataSource();
+			setDataSource("logSlave");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static String getDataSource() {
+		return (String) contextHolder.get();
+	}
+
+	public static void clearDataSource() {
+		contextHolder.remove();
+	}
+}
